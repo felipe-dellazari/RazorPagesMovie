8f1f0047d4a8674d1a4a5cf18d5153b8f6a3a78f
Felipe Dellazari <felipe.dellazari@outlook.com>
2020-01-03 00:25:32 -0300

    Concluída a adição do filtro por título e gênero. Adicionando novo campo "Rating". Adicionadas validações ao modelo.

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -24,16 +24,28 @@ namespace RazorPagesMovie
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
         public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie 
                          select m;
             if (!string.IsNullOrEmpty(SearchString))
             {
-                movies = movies.Where(s => s.Title.Contains(SearchString));
+                movies = movies.Where(x => x.Title.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
         }
     }
